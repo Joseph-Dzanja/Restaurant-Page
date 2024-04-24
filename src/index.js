e7df8b0e9f326a8f0e36c4b649ecad41a94acc37
Joseph Dzanja <jhdzanja@gmail.com>
2024-04-24 17:08:07 +0200

    somewhat responsive design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+import icon from './beer.svg';
+
+let open_nav_btn = document.querySelector('.open-btn');
+open_nav_btn.addEventListener('click', ()=>{
+    let bottom_nav = document.querySelector('.bottom-nav');
+    bottom_nav.classList.toggle('appear');
+})
+
+let home_btn = document.querySelector('.home-btn');
+const myicon = new Image();
+myicon.src = icon;
+myicon.alt = 'beerImg';
+home_btn.appendChild(myicon);
